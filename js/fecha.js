ebd450b49208c1d95cf405cc21dde688a27de5a4
Matias <matiasescobar@yahoo.com.ar>
2023-10-19 17:09:07 -0300

    se cancelo usar api de dolar por los derechos, pero se utiliza api del tiempo

--- js/fecha.js
+++ js/fecha.js
@@ -7,6 +7,9 @@ const now = DateTime.now();
 // Muestra la fecha en un elemento HTML con el id "fechaActual"
 const fechaActualElement = document.getElementById("fechaActual");
 fechaActualElement.textContent = now.toLocaleString({
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
     day: "numeric",
     month: "long",
     year: "numeric",
